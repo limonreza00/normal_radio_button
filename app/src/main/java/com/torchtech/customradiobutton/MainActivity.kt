f96d564254b557cacfe72cb24404452ed5bcb6f2
Limon <limonreza3.1416@gmail.com>
2024-03-08 16:55:50 +0600

    Initial commit

---
+++ app/src/main/java/com/torchtech/customradiobutton/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.torchtech.customradiobutton
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val genderDisplay = findViewById<TextView>(R.id.genderDisplay)
+        val radioGroup1 = findViewById<RadioGroup>(R.id.radioGroup1)
+        val showButton = findViewById<Button>(R.id.showBtn)
+
+        showButton.setOnClickListener {
+            val selectedButtonId = radioGroup1.checkedRadioButtonId
+            val selectedButton = findViewById<RadioButton>(selectedButtonId)
+            val genderName = selectedButton.text.toString()
+            genderDisplay.text= getString(R.string.your_gender_is, genderName)
+
+
+        }
+
+
+
+    }
+}
